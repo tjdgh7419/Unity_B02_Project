ffebd75236e6a3c4b2f0a72e8adebe251f1e42f8
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-08 15:12:38 +0900

    audio bgm sound set
    배경음악의 볼륨을 0.4로 줄이고 타이머가 20초가 지났을 경우 배경음악 변경

--- Unity_Card/Assets/Scripts/audioManager.cs
+++ Unity_Card/Assets/Scripts/audioManager.cs
@@ -7,14 +7,22 @@ public class audioManager : MonoBehaviour
 	public AudioSource audioSource;
 	public AudioClip bgmusic;
     public AudioClip endmusic;
+	bool timerChk = false;
 	// Start is called before the first frame update
 	void Start()
     {
         audioSource.clip = bgmusic;
+		audioSource.volume = 0.4f;
 		audioSource.Play();
 	}
     // Update is called once per frame
     void Update()
     {		
+		if(gameManager.I.time >= 20f && !timerChk)
+		{
+			timerChk = true;
+			audioSource.clip = endmusic;
+			audioSource.Play();
+		}
 	}
 }
